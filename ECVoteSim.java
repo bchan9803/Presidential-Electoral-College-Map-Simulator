b36bd49c509dbec963e4447c5b8ccb43f166475a
Bryan Chan <103339794+bchan9803@users.noreply.github.com>
2023-08-28 11:19:14 -0700

    vc

--- ECVoteSim.java
+++ ECVoteSim.java
@@ -6,43 +6,42 @@ import java.util.Map;
 class ECVoteSim {
   public static void main(String[] args) {
 
+    // Lines 9-43 parses the contents from the .CSV file, and adds them into the electoralCollegeMap hashmap
     String file = "./USElectoralMap.csv";
     BufferedReader reader = null;
     String line = "";
 
-    HashMap<String, Integer> electoralCollegeMap = new HashMap<String, Integer>();
+    HashMap<String, Integer> electoralCollegeMap = new HashMap<String, Integer>();    // Will store each state with their number of votes
 
     try {
+      // Iterates through entire .CSV file until its empty
       reader = new BufferedReader(new FileReader(file));
       while ((line = reader.readLine()) != null) {
         String[] parts = line.split(",");
 
+        // Divides the contents of the .CSV file into two parts: state and electoralVote
         String state = parts[0].trim();
         String electoralVoteStr = parts[1].trim();
 
+        // If the contents of the .CSV file are not empty, insert them into the electoralCollegeMap hashmap, but exclude the .CSV file's header
         if (!state.equals("") && !state.equals("state") && !electoralVoteStr.equals("")
             && !electoralVoteStr.equals("vote")) {
           Integer electoralVoteInt = Integer.parseInt(electoralVoteStr);
           electoralCollegeMap.put(state, electoralVoteInt);
         }
       }
-    } catch (Exception e) {
+    } catch (Exception e) {     // Print exception if error is found
       e.printStackTrace();
     } finally {
       try {
-        reader.close();
+        reader.close();           // Close the reader buffer when done
       } catch (IOException e) {
         e.printStackTrace();
       }
     }
 
-    // For testing purposes
-    // System.out.println("\n\nelectoralCollegeMap: " + electoralCollegeMap);
-
+    // candidateVoteCount hashmap stores the total votes for each candidate
     HashMap<String, Integer> candidateVoteCount = new HashMap<String, Integer>();
-    //
-
-    // System.out.println(electoralCollegeMap);
 
     Scanner getInput = new Scanner(System.in);
 
@@ -61,7 +60,7 @@ class ECVoteSim {
     candidateVoteCount.put(candidateOne, 0);
     candidateVoteCount.put(candidateTwo, 0);
 
-    Integer stateCount = 1;
+    Integer stateCount = 1;   // Displays state count
 
     for (Map.Entry<String, Integer> state : electoralCollegeMap.entrySet()) {
 
@@ -71,12 +70,18 @@ class ECVoteSim {
 
         Integer choice = getInput.nextInt();
 
+        // if the user inputs 1, then add the current state's electoral vote into
+        // the candidate's value in the electoralCollegeMap hashmap
         if (choice == 1) {
           candidateVoteCount.replace(candidateOne, candidateVoteCount.get(candidateOne) + state.getValue());
           getUserInput = false;
+        // if the user inputs 2, then add the current state's electoral vote into
+        // the candidate's value in the electoralCollegeMap hashmap
         } else if (choice == 2) {
           candidateVoteCount.replace(candidateTwo, candidateVoteCount.get(candidateTwo) + state.getValue());
           getUserInput = false;
+        // if the user enters an integer other than 1 or 2, then the system will ask for
+        // the user's input again
         } else {
           System.out.println("\nTry again.");
           getUserInput = true;
@@ -89,15 +94,18 @@ class ECVoteSim {
     // Find out who has the most votes
     String candidateWithMostVotes = "";
     Integer candidateWithMostVotesTotal = 0;
+    // Find out who has the least votes
     String candidateWithLeastVotes = "";
     Integer candidateWithLeastVotesTotal = 0;
 
+    // sets candidateWithMostVotes to candidateOne if candidateOne has more votes than candidateTwo
     if (candidateVoteCount.get(candidateOne) > candidateVoteCount.get(candidateTwo)) {
       candidateWithMostVotes = candidateOne;
       candidateWithMostVotesTotal = candidateVoteCount.get(candidateOne);
       candidateWithLeastVotes = candidateTwo;
       candidateWithLeastVotesTotal = candidateVoteCount.get(candidateTwo);
 
+    // sets candidateWithMostVotes to candidateTwo if candidateTwo has more votes than candidateOne
     } else if (candidateVoteCount.get(candidateTwo) > candidateVoteCount.get(candidateOne)) {
       candidateWithMostVotes = candidateTwo;
       candidateWithMostVotesTotal = candidateVoteCount.get(candidateTwo);
@@ -107,6 +115,8 @@ class ECVoteSim {
 
     System.out.println("\n================== RESULTS ==================\n");
 
+    // If candidateWithMostVotes does not have at least 270 electoral votes, then they have
+    // not won the presidency, and the vote will be sent to the House of Rep.
     if (candidateWithMostVotesTotal >= 270) {
       System.out.println(candidateWithMostVotes + " won the presidency with " + candidateWithMostVotesTotal + " votes!");
     } else {
